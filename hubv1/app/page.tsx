7fe41467820d19364bd94439590f3f57b01f1c32
shansrik <75145071+shansrik@users.noreply.github.com>
2025-03-06 12:45:25 -0500

    side by side
    working

--- hubv1/app/page.tsx
+++ hubv1/app/page.tsx
@@ -1,9 +1,10 @@
 import TechnicalReport from "@/components/technical-report"
+import ReportEditor from "@/components/report-editor"
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-gray-100">
-      <TechnicalReport />
+      <ReportEditor />
     </main>
   )
 }
